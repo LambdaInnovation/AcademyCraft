76f929954572caa71dd1241c7e0b824ec7a4707d
KSkun <ks@ksmeow.moe>
2015-02-10 19:11:30 +0900

    Add some recipes

--- src/main/java/cn/academy/core/register/ACRecipes.java
+++ src/main/java/cn/academy/core/register/ACRecipes.java
@@ -1,5 +1,7 @@
 package cn.academy.core.register;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -9,7 +11,18 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ACRecipes {
 
 	public static void regRecipe() {
-		
+		GameRegistry.addRecipe(new ItemStack(ACItems.coin, 12), "aaa", "bbb", "aaa", 
+				'a', ACItems.ingotNi, 'b', Items.iron_ingot);
+		GameRegistry.addRecipe(new ItemStack(ACItems.humanMachine), "aba", "aca", "ada",
+				'a', ACItems.ingotNi, 'b', Blocks.glass_pane, 'c', ACItems.pcb, 'd', ACItems.crystal);
+		GameRegistry.addRecipe(new ItemStack(ACItems.ioPort), "aaa", "aba", " a ",
+				'a', ACItems.ingotNi, 'b', ACItems.pcb);
+		GameRegistry.addRecipe(new ItemStack(ACItems.brainBeta), "aba", "bcb", "cbd",
+				'a', Items.repeater, 'b', Items.gold_ingot, 'c', Items.ender_eye, 'd', ACItems.crystal,
+				'e', ACItems.ingotNi);
+		GameRegistry.addRecipe(new ItemStack(ACBlocks.solarGen), "aaa", "bcb", "ded",
+				'a', ACItems.siliconRod, 'b', ACItems.ingotMg, 'c', ACItems.pcb, 'd', ACItems.ingotAl,
+				'e', ACItems.ingotNi);
 	}
 	
 	public static void regSmelting() {
@@ -18,6 +31,8 @@ public class ACRecipes {
 		GameRegistry.addSmelting(ACBlocks.oreCopper, new ItemStack(ACItems.ingotCu), 0.7f);
 		GameRegistry.addSmelting(ACBlocks.oreMg, new ItemStack(ACItems.ingotMg), 0.8f);
 		GameRegistry.addSmelting(ACBlocks.oreNi, new ItemStack(ACItems.ingotNi), 0.8f);
+		GameRegistry.addSmelting(ACBlocks.oreTin, new ItemStack(ACItems.ingotTin), 0.7f);
+		GameRegistry.addSmelting(ACBlocks.oreCrystal, new ItemStack(ACItems.crystal), 1.2f);
 		GameRegistry.addSmelting(ACBlocks.oreTin, new ItemStack(ACItems.ingotTin), 0.7f);
 	}
 	
