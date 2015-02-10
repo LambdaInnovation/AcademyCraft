e83aca957e2cf133ea124dc79f00614c9bf81779
mkpoli <snowwolf1001@gmail.com>
2015-02-10 16:33:19 +0800

    Basic Structure of CatTeleport

---
+++ src/main/java/cn/academy/ability/teleport/CatTeleport.java
@@ -0,0 +1,49 @@
+package cn.academy.ability.teleport;
+
+import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import cn.academy.api.ability.Category;
+import cn.academy.api.ability.Level;
+import cn.academy.api.ability.SkillBase;
+import cn.academy.core.proxy.ACClientProps;
+import cn.academy.core.register.AbilityRegistration.RegAbility;
+import cn.annoreg.core.RegistrationClass;
+
+/**
+ * 
+ * @author mkpoli_
+ *
+ */
+@RegistrationClass
+@RegAbility
+public class CatTeleport extends Category {
+	public static CatTeleport INSTANCE;
+	public final static ResourceLocation TELE_LOGO = new ResourceLocation("academy:textures/abilities/teleport/main.png");
+	
+	@Override
+	protected void register() {
+		INSTANCE = this;
+		
+		// TODO : Edit data
+		this.addLevel(new Level(this, 800.0f, 1800.0f, 0.5f, 1.0f, .9));
+		this.addLevel(new Level(this, 2000.0f, 3000.0f, 1.5f, 1.8f, .8));
+		this.addLevel(new Level(this, 3500.0f, 5500.0f, 2.2f, 2.6f, .7));
+		this.addLevel(new Level(this, 6000.0f, 9000.0f, 3.0f, 3.5f, .4));
+		this.addLevel(new Level(this, 10000.0f, Float.MAX_VALUE, 4.0f, 5.0f, .2));
+		
+		this.addSkill(new SkillBase(), 0);
+//		this.addSkill(skill, 0);
+	}
+	
+	
+	@SideOnly(Side.CLIENT)
+	public ResourceLocation getLogo() {
+		return TELE_LOGO;
+	}
+	
+	@Override
+	public String getInternalName() {
+		return "teleport";
+	}
+}
