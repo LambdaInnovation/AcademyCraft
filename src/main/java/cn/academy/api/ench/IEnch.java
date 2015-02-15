31ee583cd554a3214ac746e06ecdf93de5c026f1
KSkun <ks@ksmeow.moe>
2015-02-15 13:24:09 +0900

    EAPI Preparation

---
+++ src/main/java/cn/academy/api/ench/IEnch.java
@@ -0,0 +1,26 @@
+package cn.academy.api.ench;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * Timing-enchantment Data
+ * @author KSkun
+ */
+public abstract class IEnch {
+	
+	/** Fire Level */
+	public int fire;
+	
+	/** Vanish Level (Not in use) */
+	public float vanish;
+	
+	/** Durability Improvement Level */
+	public float durability;
+	
+	/** Damage Improvement Level */
+	public float damage;
+	
+	/** Data ID */
+//	public int id;
+	
+}
