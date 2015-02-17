7ad849b121baa8e8d2a8505d316fd4ea13835758
KSkun <ks@ksmeow.moe>
2015-02-17 22:26:29 +0900

    Fix some bugs in recipe system

--- src/main/java/cn/academy/core/register/ACRecipes.java
+++ src/main/java/cn/academy/core/register/ACRecipes.java
@@ -13,11 +13,11 @@ public class ACRecipes {
 	public static void regRecipe() {
 		GameRegistry.addRecipe(new ItemStack(ACItems.coin, 12), "aaa", "bbb", "aaa", 
 				'a', ACItems.ingotNi, 'b', Items.iron_ingot);
-		GameRegistry.addRecipe(new ItemStack(ACItems.humanMachine), "aba", "aca", "ada",
+		GameRegistry.addRecipe(new ItemStack(ACItems.bodyDet), "aba", "aca", "ada",
 				'a', ACItems.ingotNi, 'b', Blocks.glass_pane, 'c', ACItems.pcb, 'd', ACItems.crystal);
 		GameRegistry.addRecipe(new ItemStack(ACItems.ioPort), "aaa", "aba", " a ",
 				'a', ACItems.ingotNi, 'b', ACItems.pcb);
-		GameRegistry.addRecipe(new ItemStack(ACItems.brainBeta), "aba", "bcb", "cbd",
+		GameRegistry.addRecipe(new ItemStack(ACItems.brainBeta), "aba", "bcb", "dbe",
 				'a', Items.repeater, 'b', Items.gold_ingot, 'c', Items.ender_eye, 'd', ACItems.crystal,
 				'e', ACItems.ingotNi);
 		GameRegistry.addRecipe(new ItemStack(ACBlocks.solarGen), "aaa", "bcb", "ded",
@@ -37,27 +37,26 @@ public class ACRecipes {
 				'a', ACItems.ingotMg);	
 		GameRegistry.addShapelessRecipe(new ItemStack(ACItems.almgPlate, 2), 
 				ACItems.alPlate, ACItems.mgPlate);
-		GameRegistry.addRecipe(new ItemStack(ACBlocks.node), "aaa", "bcb", "ded",
-				'a', ACItems.siliconRod, 'b', ACItems.ingotMg, 'c', ACItems.pcb, 'd', ACItems.ingotAl,
-				'e', ACItems.ingotNi);
+		GameRegistry.addRecipe(new ItemStack(ACBlocks.node), "aba", "cdc", "aba",
+				'a', Blocks.planks, 'b', Items.iron_ingot, 'c', ACItems.energyCrystal, 'd', ACItems.pcb);
 		GameRegistry.addRecipe(new ItemStack(ACItems.copperCoil, 6), "aba", "b a", "aba",
 				'a', Items.iron_ingot, 'b', ACItems.ingotNi);
 		GameRegistry.addRecipe(new ItemStack(ACBlocks.magInducer), " a ", "aba", " a ",
 				'a', ACItems.copperCoil, 'b', ACItems.pcb);
 		GameRegistry.addRecipe(new ItemStack(ACBlocks.grid), "aba", "cdc", "aba",
 				'a', Blocks.glass_pane, 'b', ACItems.pcb, 'c', ACItems.almgPlate, 'd', ACItems.energyCrystal);
-		GameRegistry.addShapelessRecipe(new ItemStack(ACItems.compWood), new ItemStack(Blocks.planks, 3));
+//		GameRegistry.addShapelessRecipe(new ItemStack(ACItems.compPlank), new ItemStack(Blocks.planks, 3));
 		GameRegistry.addRecipe(new ItemStack(ACItems.expNail, 8), "aba", "bcb", "dad",
-				'a', Items.iron_ingot, 'b', ACItems.compWood, 'c', Items.gunpowder, 'd', Items.slime_ball);
+				'a', Items.iron_ingot, 'b', ACItems.compPlank, 'c', Items.gunpowder, 'd', Items.slime_ball);
 		GameRegistry.addRecipe(new ItemStack(ACItems.pcb), "aba", "cdc", "aba",
 				'a', ACItems.ingotNi, 'b', ACItems.crystal, 'c', Items.redstone, 'd', Items.iron_ingot);
-		GameRegistry.addRecipe(new ItemStack(ACItems.devModule), "aba", "cdc", "efe",
-				'a', ACItems.ioPort, 'b', ACItems.humanMachine, 'c', ACItems.brainBeta, 'd', ACItems.pcb,
+		GameRegistry.addRecipe(new ItemStack(ACItems.aimCell), "aba", "cdc", "efe",
+				'a', ACItems.ioPort, 'b', ACItems.bodyDet, 'c', ACItems.brainBeta, 'd', ACItems.pcb,
 				'e', ACBlocks.node, 'f', ACItems.almgPlate);
 	}
 	
 	public static void regSmelting() {
-		GameRegistry.addSmelting(new ItemStack(Blocks.glass, 3), new ItemStack(ACItems.siliconRod), 0f);
+//		GameRegistry.addSmelting(new ItemStack(Blocks.glass, 3), new ItemStack(ACItems.siliconRod), 0f);
 		//Ore Smelting
 		GameRegistry.addSmelting(ACBlocks.oreAl, new ItemStack(ACItems.ingotAl), 0.7f);
 		GameRegistry.addSmelting(ACBlocks.oreCopper, new ItemStack(ACItems.ingotCu), 0.7f);
