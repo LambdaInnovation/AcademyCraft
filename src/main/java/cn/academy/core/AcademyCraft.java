7b6e2123a6e441c84795f8e36e96007cd71c4325
Wu Zhenwei <wuzhenwei35@gmail.com>
2015-03-24 23:48:35 +0800

    Using new version system.

--- src/main/java/cn/academy/core/AcademyCraft.java
+++ src/main/java/cn/academy/core/AcademyCraft.java
@@ -44,14 +44,9 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
  * @author acaly, WeathFolD, KS
  *
  */
-@Mod(modid = "academy-craft", name = "AcademyCraft", version = AcademyCraft.VERSION)
+@Mod(modid = "academy-craft", name = "AcademyCraft")
 @RegistrationMod(pkg = "cn.academy.", res = "academy", prefix = "ac_")
 public class AcademyCraft {
-
-	/**
-	 * 当前版本
-	 */
-	public static final String VERSION = "1.0beta2_pre1";
 
 	public static final String NET_CHANNEL = "academy-network";
 
@@ -84,7 +79,7 @@ public class AcademyCraft {
 		config = new Configuration(event.getSuggestedConfigurationFile());
 		RegistrationManager.INSTANCE.registerAll(this, "PreInit");
 		
-		log.info("Starting AcademyCraft " + VERSION);
+		log.info("Starting AcademyCraft");
 		log.info("Copyright (c) Lambda Innovation, 2013-2015");
 		log.info("http://ac.li-dev.cn/");
 	}
