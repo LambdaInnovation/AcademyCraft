12067f71848dc791b33dc8aeb8391422888f3f4b
KSkun <ks@ksmeow.moe>
2015-03-29 20:17:23 +0800

    Set mineral gen data

--- src/main/java/cn/academy/misc/world/ACWorldGen.java
+++ src/main/java/cn/academy/misc/world/ACWorldGen.java
@@ -40,11 +40,11 @@ public class ACWorldGen implements IWorldGenerator {
 	}
 	
 	private CustomGen[] generators = new CustomGen[] {
-	        new CustomGen(new WorldGenMinable(ACBlocks.oreAl, 0, 10, Blocks.stone), 60, 5),
-	        new CustomGen(new WorldGenMinable(ACBlocks.oreMg, 0, 10, Blocks.stone), 60, 5),
-	        new CustomGen(new WorldGenMinable(ACBlocks.oreNi, 0, 10, Blocks.stone), 60, 5),
-	        new CustomGen(new WorldGenMinable(ACBlocks.oreShadow, 0, 10, Blocks.stone), 60, 5, shadowGenIds),
-	        new CustomGen(new WorldGenMinable(ACBlocks.oreCrystal, 0, 10, Blocks.stone), 30, 5),
+	        new CustomGen(new WorldGenMinable(ACBlocks.oreAl, 0, 6, Blocks.stone), 60, 30),
+	        new CustomGen(new WorldGenMinable(ACBlocks.oreMg, 0, 3, Blocks.stone), 60, 28),
+	        new CustomGen(new WorldGenMinable(ACBlocks.oreNi, 0, 5, Blocks.stone), 60, 28),
+	        new CustomGen(new WorldGenMinable(ACBlocks.oreShadow, 0, 2, Blocks.stone), 60, 15, shadowGenIds),
+	        new CustomGen(new WorldGenMinable(ACBlocks.oreCrystal, 0, 2, Blocks.stone), 40, 22),
 	};
 	
 	private void genOverworld(World world, Random random, int x, int z) {
