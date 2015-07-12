13318ea2b42d63f160b02e7b84b8abaaba3703d2
ShieLian <947514447@qq.com>
2015-07-12 19:49:04 +0800

    Fix Sycn Problem

--- src/main/java/cn/academy/vanilla/heatmaster/skill/WorldHeater.java
+++ src/main/java/cn/academy/vanilla/heatmaster/skill/WorldHeater.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
@@ -21,6 +22,9 @@ import cn.academy.vanilla.electromaster.skill.ArcGen;
 import cn.academy.vanilla.electromaster.skill.ArcGen.ArcGenAction;
 import cn.academy.vanilla.heatmaster.entity.EntityWorldHeater;
 import cn.liutils.entityx.handlers.Life;
+import cn.liutils.util.mc.BlockFilters;
+import cn.liutils.util.mc.EntitySelectors;
+import cn.liutils.util.mc.EntitySelectors.SelectorOfType;
 import cn.liutils.util.raytrace.Raytrace;
 
 public class WorldHeater extends Skill
@@ -30,6 +34,9 @@ public class WorldHeater extends Skill
 	static
 	{
 		heatmap.put(Block.getBlockFromName("sand"),Block.getBlockFromName("glass"));
+//		heatmap.put(Block.getBlockFromName("sand"),Block.getBlockFromName("glass"));
+//		heatmap.put(Block.getBlockFromName("sand"),Block.getBlockFromName("glass"));
+//		heatmap.put(Block.getBlockFromName("sand"),Block.getBlockFromName("glass"));
 		//TODO add more
 		// Suggestion: Maybe you can access minecraft's melting data? Weathfold
 		// Reply: Yiya~ exp will be ignored. ShieLian
@@ -58,6 +65,19 @@ public class WorldHeater extends Skill
 			AbilityData aData= AbilityData.get(player);
 			CPData cpData= CPData.get(player);
 
+			boolean flag= false;
+
+			// Perform ray trace
+			// TODO add ripple support --8(distance)
+			MovingObjectPosition result=Raytrace.traceLiving(player, 8,null, BlockFilters.filNothing);
+			if (result!=null&&result.typeOfHit ==MovingObjectPosition.MovingObjectType.BLOCK)
+				flag= true;
+			if(!flag) return false;
+			
+			Block block=player.worldObj.getBlock(result.blockX,result.blockY,result.blockZ);
+			flag=heatmap.keySet().contains(block);
+			if(!flag) return false;
+			
 			return cpData.perform(getOverload(aData),getConsumption(aData));
 		}
 
@@ -69,8 +89,9 @@ public class WorldHeater extends Skill
 
 			// Perform ray trace
 			// TODO add ripple support --8(distance)
-			MovingObjectPosition result= player.rayTrace(8,1.0F);
-			if (result.typeOfHit ==MovingObjectPosition.MovingObjectType.BLOCK)
+			//MovingObjectPosition result= player.rayTrace(8,1.0F);
+			MovingObjectPosition result=Raytrace.traceLiving(player, 8,null, BlockFilters.filNothing);
+			if (result!=null&&result.typeOfHit ==MovingObjectPosition.MovingObjectType.BLOCK)
 				flag= true;
 			if(!flag) return;
 			
