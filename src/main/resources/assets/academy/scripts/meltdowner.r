4c485f71ca3cfd7211489cf8e4cfa5fcfa0922af
Eing0 <984982794@qq.com>
2015-10-19 15:57:43 +0800

    Update meltdowner.r

--- src/main/resources/assets/academy/scripts/meltdowner.r
+++ src/main/resources/assets/academy/scripts/meltdowner.r
@@ -3,9 +3,9 @@ ac {
 		# 电子光束
         electron_bomb {
             x { 106 } y { 202 }
-            damage(exp) { lerp(5, 10, exp) }
-            consumption(exp) { lerp(35, 60, exp) }
-            overload(exp) { lerp(16, 11, exp) }
+            damage(exp) { lerp(9, 14, exp) }
+            consumption(exp) { lerp(117, 135, exp) }
+            overload(exp) { lerp(39, 17, exp) }
             cooldown(exp) { lerp(20, 10, exp) }
             expincr { 0.005 }
         }
@@ -15,23 +15,23 @@ ac {
             x { 212 } y { 315 }
 			
 			# 伤害的强化倍率
-            rate(exp) { lerp(1.4, 2, exp) }
+            rate(exp) { lerp(1.4, 1.8, exp) }
         }
 		
 		# 电子弹散射
         scatter_bomb {
             x { 308 } y { 70 }
-            damage(exp) { lerp(5, 9, exp) }
-            consumption(exp) { lerp(3, 6, exp) }
-            overload(exp) { lerp(100, 80, exp) }
+            damage(exp) { lerp(4, 6, exp) }
+            consumption(exp) { lerp(7, 9, exp) }
+            overload(exp) { lerp(165, 68, exp) }
             expincr { 0.001 }
         }
         
 		# 光盾
         light_shield {
             x { 391 } y { 266 }
-            consumption(exp) { lerp(9, 4, exp) }
-            overload(exp) { lerp(90, 60, exp) }
+            consumption(exp) { lerp(12, 7, exp) }
+            overload(exp) { lerp(198, 132, exp) }
             cooldown(exp) { lerp(80, 60, exp) }
             
 			# 伤害吸收一次的消耗或过载
@@ -39,9 +39,9 @@ ac {
             absorb_overload(exp) { lerp(15, 10, exp) }
             
 			# 攻击到实体给予的伤害
-            touch_damage(exp) { lerp(2, 6, exp) }
+            touch_damage(exp) { lerp(3, 8, exp) }
 			# 最大吸收的伤害值
-            absorb_damage(exp) { lerp(8, 25, exp) }
+            absorb_damage(exp) { lerp(15, 50, exp) }
             
 			# 在使用一次光盾、被实体攻击一次、给予实体攻击一次时都增加经验
             expincr { 0.001 }
@@ -56,11 +56,11 @@ ac {
 			# 光束半径
             range(exp) { lerp(2, 3, exp) }
 			# 方块破坏能量
-            energy(exp, time) { rate(time) * lerp(600, 1300, exp) }
-            damage(exp, time) { rate(time) * lerp(18, 45, exp) }
-            overload(exp) { lerp(200, 170, exp) }
-            consumption(exp) { lerp(10, 13, exp) }
-            cooldown(exp, time) { rate(time) * 20 * lerp(15, 6, exp) }
+            energy(exp, time) { rate(time) * lerp(300, 700, exp) }
+            damage(exp, time) { rate(time) * lerp(20, 50, exp) }
+            overload(exp) { lerp(300, 200, exp) }
+            consumption(exp) { lerp(15, 27, exp) }
+            cooldown(exp, time) { rate(time) * 20 * lerp(15, 7, exp) }
             expincr(time) { rate(time) * 0.002 }
         }
         
@@ -75,8 +75,8 @@ ac {
             range { 10 }
             harvest_level { 2 }
             speed(exp) { 0.2 * lerp(1, 2, exp) }
-            consumption(exp) { lerp(15, 20, exp) }
-            overload(exp) { lerp(150, 70, exp) }
+            consumption(exp) { lerp(20, 15, exp) }
+            overload(exp) { lerp(230, 140, exp) }
             cooldown(exp) { lerp(60, 30, exp) }
             expincr { 0.0005 }
         }
@@ -87,9 +87,9 @@ ac {
 			# 没有射到Silbarn时的光束伤害
             plain_damage(exp) { lerp(25, 40, exp) }
 			# 散射时每个光束的伤害
-            scatter_damage(exp) { lerp(16, 24, exp) }
-            consumption(exp) { lerp(300, 200, exp) }
-            overload(exp) { lerp(350, 300, exp) }
+            scatter_damage(exp) { lerp(12, 20, exp) }
+            consumption(exp) { lerp(450, 340, exp) }
+            overload(exp) { lerp(375, 160, exp) }
             cooldown(exp) { 20 * lerp(5, 8, exp) }
             expincr { 0.005 }
         }
@@ -97,10 +97,10 @@ ac {
 		# 喷气引擎
         jet_engine {
             x { 654 } y { 263 }
-            damage(exp) { lerp(20, 30, exp) }
+            damage(exp) { lerp(15, 35, exp) }
             cooldown(exp) { 18 * floor(lerp(6, 3, exp)) }
-            consumption(exp) { 1.5 * lerp(170, 140, exp) }
-            overload(exp) { 1.3 * lerp(60, 50, exp) }
+            consumption(exp) { lerp(170, 140, exp) }
+            overload(exp) { lerp(66, 42, exp) }
             expincr { 0.004 }
         }
         
@@ -110,8 +110,8 @@ ac {
             range { 20 }
             harvest_level { 5 }
             speed(exp) { 0.5 * lerp(1, 2, exp) }
-            consumption(exp) { lerp(18, 12, exp) }
-            overload(exp) { lerp(200, 110, exp) }
+            consumption(exp) { lerp(25, 20, exp) }
+            overload(exp) { lerp(300, 180, exp) }
             cooldown(exp) { lerp(60, 30, exp) }
             expincr { 0.0003 }
         }
@@ -122,8 +122,8 @@ ac {
             range { 20 }
             harvest_level { 5 }
             speed(exp) { 0.5 * lerp(1, 2, exp) }
-            consumption(exp) { lerp(20, 15, exp) }
-            overload(exp) { lerp(250, 160, exp) }
+            consumption(exp) { lerp(50, 40, exp) }
+            overload(exp) { lerp(400, 300, exp) }
             cooldown(exp) { lerp(60, 30, exp) }
             expincr { 0.0003 }
         }
@@ -136,13 +136,13 @@ ac {
 			# 固有过载
             overload(exp) { lerp(2, 1.5, exp) }
 			# 发射一发光束时过载
-            overload_attacked(exp) { lerp(50, 35, exp)}
+            overload_attacked(exp) { lerp(61, 32, exp)}
 			# 发射一发光束时消耗
-            consumption_attacked(exp) { lerp(300, 500, exp) }
+            consumption_attacked(exp) { lerp(270, 405, exp) }
             cooldown(ticks) { min(300, max(100, ticks)) }
 			# 最大持续时间
             time_limit(exp) { 20 * lerp(7, 20, exp) }
-            damage(exp) { lerp(14, 22, exp) }
+            damage(exp) { lerp(14, 27, exp) }
             expincr { 0.001 } 
         }
     }
