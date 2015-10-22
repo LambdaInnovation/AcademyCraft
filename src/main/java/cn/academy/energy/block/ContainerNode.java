738536af25fcf8f606a38b23eb001825c9e55ea8
panfengj <panfengjason@gmail.com>
2015-10-22 16:56:33 +0800

    Revert "Fix Issue #188"
    This reverts commit 8bc2319f22e2ef2f4bdd3880e484099270b246ad.

--- src/main/java/cn/academy/energy/block/ContainerNode.java
+++ src/main/java/cn/academy/energy/block/ContainerNode.java
@@ -38,8 +38,8 @@ public class ContainerNode extends Container {
     }
     
     private void initInventory(InventoryPlayer inv) {
-        this.addSlotToContainer(new SlotIFItem(node, 0, 38, 71));
-        this.addSlotToContainer(new SlotIFItem(node, 1, 78, 71));
+        this.addSlotToContainer(new SlotIFItem(node, 0, 38, 72));
+        this.addSlotToContainer(new SlotIFItem(node, 1, 78, 72));
         
         int STEP = 18;
         
