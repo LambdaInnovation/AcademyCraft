074513decabc97d212d09be9d8ef46759c039693
WeAthFolD <cheetah77@qq.com>
2015-10-22 17:03:06 +0800

    Prepare for an internal build

---
+++ src/main/java/cn/academy/ability/api/data/DataHooks.java
@@ -0,0 +1,36 @@
+package cn.academy.ability.api.data;
+
+import cn.academy.core.AcademyCraft;
+import cn.academy.core.util.SubscribePipeline;
+import cn.annoreg.core.Registrant;
+import cn.annoreg.mc.RegInit;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Registrant
+@RegInit
+public class DataHooks {
+	
+	public static void init() {
+		AcademyCraft.pipeline.register(new DataHooks());
+	}
+	
+	// TODO Dynamicify this pattern. That is, allow range and other parameters to go like this without sucking up the code
+
+	@SubscribePipeline("?.?.$damage")
+	public float modifyDamage(float value, EntityPlayer player) {
+		AbilityData data = AbilityData.get(player);
+		return value * data.getCategory().getDamageRate(data);
+	}
+	
+	@SubscribePipeline("?.?.$consumption")
+	public float modifyConsumption(float value, EntityPlayer player) {
+		AbilityData data = AbilityData.get(player);
+		return value * data.getCategory().getConsumptionRate(data);
+	}
+	
+	@SubscribePipeline("?.?.$overload")
+	public float modifyOverload(float value, EntityPlayer player) {
+		AbilityData data = AbilityData.get(player);
+		return value * data.getCategory().getOverloadRate(data);
+	}
+}
