35e6feb9c2205f9379a0c22945b5d72b3bd6b8a3
Administrator <Administrator@StuA-044>
2015-10-28 21:27:12 +0800

    加上了damageID和meta的支持
    但是是得在注册条件的时候手动制定值
    
    对没有子物品的物品制定damageID不会产生任何作用，不会有标识的数字
    
    暂时先这样push！

--- src/main/java/cn/academy/misc/tutorial/Condition.java
+++ src/main/java/cn/academy/misc/tutorial/Condition.java
@@ -27,6 +27,7 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 
 //@Registrant
 public abstract class Condition {
@@ -52,6 +53,7 @@ public abstract class Condition {
 			data.allSaved[index]=true;
 			data.update();
 		}
+		
 	}
 
 	static class AbilityLevelCondition extends Condition{
@@ -81,22 +83,31 @@ public abstract class Condition {
 		static HashMap<String,ItemCondition> smeltMap = new HashMap<String,ItemCondition>();
 		static HashMap<MatterMaterial,ItemCondition> matterUnitMap = new HashMap<MatterMaterial,ItemCondition>();
 		
+		static String getKeyFromItemStack(ItemStack i){
+			String key=i.getUnlocalizedName();
+			if(i.getItem().getHasSubtypes())
+				key+=i.getItemDamage();
+			else
+				key+="0";
+			return key;
+		}
+		
 		@SubscribeEvent
 		public void onItemCrafted(ItemCraftedEvent e){
-			Item i = e.crafting.getItem();
-			if(craftMap.containsKey(i.getUnlocalizedName()))craftMap.get(i.getUnlocalizedName()).pass(e.player);
+			String key=getKeyFromItemStack(e.crafting);
+			if(craftMap.containsKey(key))craftMap.get(key).pass(e.player);
 		}
 		
 		@SubscribeEvent
 		public void onItemPickup(ItemPickupEvent e){
-			Item i = e.pickedUp.getEntityItem().getItem();
-			if(pickupMap.containsKey(i.getUnlocalizedName()))pickupMap.get(i.getUnlocalizedName()).pass(e.player);
+			String key=getKeyFromItemStack(e.pickedUp.getEntityItem());
+			if(pickupMap.containsKey(key))pickupMap.get(key).pass(e.player);
 		}
 		
 		@SubscribeEvent
 		public void onItemSmelted(ItemSmeltedEvent e){
-			Item i = e.smelting.getItem();
-			if(smeltMap.containsKey(i.getUnlocalizedName()))smeltMap.get(i.getUnlocalizedName()).pass(e.player);
+			String key=getKeyFromItemStack(e.smelting);
+			if(smeltMap.containsKey(key))smeltMap.get(key).pass(e.player);
 		}
 		
 		@SubscribeEvent
@@ -132,6 +143,16 @@ public abstract class Condition {
 	Condition setSaveToNBT(){
 		this.needSaveNBT=true;
 		return this;
+	}
+	
+
+	static String getKeyFromItem(Item i,int subID){
+		String key=i.getUnlocalizedName();
+		if(i.getHasSubtypes())
+			key+=subID;
+		else
+			key+="0";
+		return key;
 	}
 	
 	//=============================================================================
@@ -171,119 +192,170 @@ public abstract class Condition {
 	}
 	
 	//=============================================================================
-	
+	/**
+	 * 仅限传入的Item和Block不需要区分damageID或者meta或者对应的值是0的时候可以用这个来简tou化lan
+	 * @param itemsOrBlocks
+	 * @return
+	 * @throws Exception
+	 */
 	public static Condition[] itemsCrafted(Object...itemsOrBlocks) throws Exception{
 		List<Condition> c=new ArrayList<Condition>();
 		for(Object IorB : itemsOrBlocks){
 			Item item = null;
 			//java sucks?
-			if(IorB instanceof Item)item = (Item) IorB;
-			else if(IorB instanceof Block)item = Item.getItemFromBlock((Block) IorB);
+			if(IorB instanceof Item)
+				item = (Item) IorB;
+			else if(IorB instanceof Block)
+				item = Item.getItemFromBlock((Block) IorB);
 			else{
 				throw new Exception("Not an Item or Block: "+IorB);
 			}
-			if(HandleEvent.craftMap.containsKey(item.getUnlocalizedName())){
-				c.add(HandleEvent.craftMap.get(item.getUnlocalizedName()));
+			String key=getKeyFromItem(item, 0);
+			if(HandleEvent.craftMap.containsKey(key)){
+				c.add(HandleEvent.craftMap.get(key));
 			}else{
 				ItemCondition c0=new ItemCondition();
 				c.add(c0);
-				HandleEvent.craftMap.put(item.getUnlocalizedName(), c0);
+				HandleEvent.craftMap.put(key, c0);
 			}
 		}
 		return c.toArray(new Condition[c.size()]);
 	}
 	
-	public static Condition itemCrafted(Item item) throws Exception{
+	public static Condition itemCrafted(Item item,int damageID) throws Exception{
 		ItemCondition c;
-		if(HandleEvent.craftMap.containsKey(item.getUnlocalizedName())){
-			c=HandleEvent.craftMap.get(item.getUnlocalizedName());
+		String key=getKeyFromItem(item, damageID);
+		if(HandleEvent.craftMap.containsKey(key)){
+			c=HandleEvent.craftMap.get(key);
 		}else{
 			c=new ItemCondition();
-			HandleEvent.craftMap.put(item.getUnlocalizedName(),c);
+			HandleEvent.craftMap.put(key,c);
 		}
 		return c;
 	}
 	
-	public static Condition itemCrafted(Block block) throws Exception{
+	public static Condition itemCrafted(Item item) throws Exception{
+		return itemCrafted(item, 0);
+	}
+	
+	public static Condition itemCrafted(Block block,int meta) throws Exception{
 		Item item = Item.getItemFromBlock(block);
-		return itemCrafted(item);
+		return itemCrafted(item,meta);
+	}
+	
+	public static Condition itemCrafted(Block block) throws Exception{
+		return itemCrafted(block, 0);
 	}
 	
 	//=============================================================================
-	
+	/**
+	 * 仅限传入的Item和Block不需要区分damageID或者meta或者对应的值是0的时候可以用这个来简tou化lan
+	 * @param itemsOrBlocks
+	 * @return
+	 * @throws Exception
+	 */
 	public static Condition[] itemsPickup(Object...itemsOrBlocks) throws Exception{
 		List<Condition> c=new ArrayList<Condition>();
 		for(Object IorB : itemsOrBlocks){
 			Item item = null;
 			//java sucks?
-			if(IorB instanceof Item)item = (Item) IorB;
-			else if(IorB instanceof Block)item = Item.getItemFromBlock((Block) IorB);
+			if(IorB instanceof Item)
+				item = (Item) IorB;
+			else if(IorB instanceof Block)
+				item = Item.getItemFromBlock((Block) IorB);
 			else{
 				throw new Exception("Not an Item or Block: "+IorB);
 			}
-			if(HandleEvent.pickupMap.containsKey(item.getUnlocalizedName())){
-				c.add(HandleEvent.pickupMap.get(item.getUnlocalizedName()));
+			String key=getKeyFromItem(item, 0);
+			if(HandleEvent.pickupMap.containsKey(key)){
+				c.add(HandleEvent.pickupMap.get(key));
 			}else{
 				ItemCondition c0=new ItemCondition();
 				c.add(c0);
-				HandleEvent.pickupMap.put(item.getUnlocalizedName(), c0);
+				HandleEvent.pickupMap.put(key, c0);
 			}
 		}
 		return c.toArray(new Condition[c.size()]);
 	}
 	
-	public static Condition itemPickup(Item item) throws Exception{
+	public static Condition itemPickup(Item item,int damageID) throws Exception{
 		ItemCondition c;
-		if(HandleEvent.pickupMap.containsKey(item.getUnlocalizedName())){
-			c=HandleEvent.pickupMap.get(item.getUnlocalizedName());
+		String key=getKeyFromItem(item, damageID);
+		if(HandleEvent.pickupMap.containsKey(key)){
+			c=HandleEvent.pickupMap.get(key);
 		}else{
 			c=new ItemCondition();
-			HandleEvent.pickupMap.put(item.getUnlocalizedName(), c);
+			HandleEvent.pickupMap.put(key, c);
 		}
 		return c;
 	}
 	
-	public static Condition itemPickup(Block block) throws Exception{
+	public static Condition itemPickup(Item item) throws Exception{
+		return itemPickup(item, 0);
+	}
+	
+	public static Condition itemPickup(Block block,int meta) throws Exception{
 		Item item = Item.getItemFromBlock(block);
-		return itemPickup(item);
+		return itemPickup(item,meta);
+	}
+	
+	public static Condition itemPickup(Block block) throws Exception{
+		return itemPickup(block,0);
 	}
 	//=============================================================================
-	
+	/**
+	 * 仅限传入的Item和Block不需要区分damageID或者meta或者对应的值是0的时候可以用这个来简tou化lan
+	 * @param itemsOrBlocks
+	 * @return
+	 * @throws Exception
+	 */
 	public static Condition[] itemsSmelted(Object...itemsOrBlocks) throws Exception{
 		List<Condition> c=new ArrayList<Condition>();
 		for(Object IorB : itemsOrBlocks){
 			Item item = null;
 			//java sucks?
-			if(IorB instanceof Item)item = (Item) IorB;
-			else if(IorB instanceof Block)item = Item.getItemFromBlock((Block) IorB);
+			if(IorB instanceof Item)
+				item = (Item) IorB;
+			else if(IorB instanceof Block)
+				item = Item.getItemFromBlock((Block) IorB);
 			else{
 				throw new Exception("Not an Item or Block: "+IorB);
 			}
-			if(HandleEvent.smeltMap.containsKey(item.getUnlocalizedName())){
-				c.add(HandleEvent.smeltMap.get(item.getUnlocalizedName()));
+			String key=getKeyFromItem(item, 0);
+			if(HandleEvent.smeltMap.containsKey(key)){
+				c.add(HandleEvent.smeltMap.get(key));
 			}else{
 				ItemCondition c0=new ItemCondition();
 				c.add(c0);
-				HandleEvent.smeltMap.put(item.getUnlocalizedName(), c0);
+				HandleEvent.smeltMap.put(key, c0);
 			}
 		}
 		return c.toArray(new Condition[c.size()]);
 	}
 	
-	public static Condition itemSmelted(Item item) throws Exception{
+	public static Condition itemSmelted(Item item,int damageID) throws Exception{
 		ItemCondition c;
-		if(HandleEvent.smeltMap.containsKey(item.getUnlocalizedName())){
-			c=HandleEvent.smeltMap.get(item.getUnlocalizedName());
+		String key=getKeyFromItem(item, damageID);
+		if(HandleEvent.smeltMap.containsKey(key)){
+			c=HandleEvent.smeltMap.get(key);
 		}else{
 			c=new ItemCondition();
-			HandleEvent.smeltMap.put(item.getUnlocalizedName(), c);
+			HandleEvent.smeltMap.put(key, c);
 		}
 		return c;
 	}
+	
+	public static Condition itemSmelted(Item item) throws Exception{
+		return itemSmelted(item, 0);
+	}
 
-	public static Condition itemSmelted(Block block) throws Exception{
+	public static Condition itemSmelted(Block block,int meta) throws Exception{
 		Item item = Item.getItemFromBlock(block);
-		return itemSmelted(item);
+		return itemSmelted(item,meta);
+	}
+	
+	public static Condition itemSmelted(Block block) throws Exception{
+		return itemSmelted(block, 0);
 	}
 	//=============================================================================
 	
