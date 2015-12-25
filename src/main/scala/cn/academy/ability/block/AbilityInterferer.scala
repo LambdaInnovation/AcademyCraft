0fb949a5fa8b7893bdc162a6b6dbc3721cab1e6b
WeAthFolD <cheetah77@qq.com>
2015-12-25 15:13:14 +0800

    Improved ability interf block using TickScheduler

--- src/main/scala/cn/academy/ability/block/AbilityInterferer.scala
+++ src/main/scala/cn/academy/ability/block/AbilityInterferer.scala
@@ -5,7 +5,9 @@ import cn.academy.ability.api.data.CPData.IInterfSource
 import cn.academy.core.block.ACBlockContainer
 import cn.lambdalib.annoreg.core.Registrant
 import cn.lambdalib.annoreg.mc.RegTileEntity
+import cn.lambdalib.util.helper.TickScheduler
 import cn.lambdalib.util.mc.{EntitySelectors, WorldUtils}
+import cpw.mods.fml.relauncher.Side
 import net.minecraft.block.material.Material
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.tileentity.TileEntity
@@ -13,48 +15,41 @@ import net.minecraft.world.World
 
 import scala.collection.JavaConversions._
 
-object AbilityInterferer {
-  @RegTileEntity
-  class Tile extends TileEntity {
+@Registrant
+@RegTileEntity
+class TileAbilityInterferer extends TileEntity {
 
-    var counter = 10
+  val scheduler = new TickScheduler
 
-    lazy val sourceName = s"interferer@${getWorldObj.provider.dimensionId}($xCoord,$yCoord,$zCoord)"
+  lazy val sourceName = s"interferer@${getWorldObj.provider.dimensionId}($xCoord,$yCoord,$zCoord)"
 
-    /**
-      * TODO make user able to specify the range
-      */
-    def range = 5
+  /**
+    * TODO make user able to specify the range
+    */
+  def range = 5
 
-    override def updateEntity() = {
-      val world = getWorldObj
-      if (!world.isRemote) {
-        counter -= 1
-
-        if (counter == 0) {
-          counter = 10
-
-          val rangeVal = range
-          val players = WorldUtils.getEntities(this, rangeVal, EntitySelectors.survivalPlayer)
-          players foreach {
-            case player: EntityPlayer =>
-              CPData.get(player).addInterf(sourceName, new IInterfSource {
-                override def interfering(): Boolean =
-                  player.getDistanceSq(xCoord+0.5, yCoord+0.5, zCoord+0.5) < rangeVal * rangeVal &&
-                  !Tile.this.isInvalid
-              })
-          }
-        }
+  scheduler.every(10).atOnly(Side.SERVER).run(new Runnable {
+    override def run() = {
+      println("Running...")
+      val rangeVal = range
+      val players = WorldUtils.getEntities(TileAbilityInterferer.this, rangeVal, EntitySelectors.survivalPlayer)
+      players foreach {
+        case player: EntityPlayer =>
+          CPData.get(player).addInterf(sourceName, new IInterfSource {
+            override def interfering(): Boolean =
+              player.getDistanceSq(xCoord+0.5, yCoord+0.5, zCoord+0.5) < rangeVal * rangeVal &&
+                !TileAbilityInterferer.this.isInvalid
+          })
       }
     }
+  })
 
-  }
+  override def updateEntity() = scheduler.runTick()
+
 }
 
-@Registrant
 class AbilityInterferer extends ACBlockContainer("ability_interferer", Material.rock) {
-  import AbilityInterferer._
 
-  override def createNewTileEntity(world: World, meta: Int) = new Tile
+  override def createNewTileEntity(world: World, meta: Int) = new TileAbilityInterferer
 
 }
