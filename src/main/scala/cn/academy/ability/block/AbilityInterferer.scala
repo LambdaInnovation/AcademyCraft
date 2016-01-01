caf4cc132b39e6b9e017a776328a0624744c060c
WeAthFolD <cheetah77@qq.com>
2016-01-02 04:27:56 +0800

    Implemented AbilityInterferer

--- src/main/scala/cn/academy/ability/block/AbilityInterferer.scala
+++ src/main/scala/cn/academy/ability/block/AbilityInterferer.scala
@@ -1,34 +1,80 @@
 package cn.academy.ability.block
 
+import java.lang
+import java.util.function.Supplier
+
 import cn.academy.ability.api.data.CPData
 import cn.academy.ability.api.data.CPData.IInterfSource
 import cn.academy.core.block.ACBlockContainer
+import cn.academy.core.client.Resources
 import cn.lambdalib.annoreg.core.Registrant
 import cn.lambdalib.annoreg.mc.RegTileEntity
+import cn.lambdalib.cgui.gui.CGuiScreen
+import cn.lambdalib.cgui.gui.component.DragBar.DraggedEvent
+import cn.lambdalib.cgui.gui.component.TextBox.ConfirmInputEvent
+import cn.lambdalib.cgui.gui.component.{TextBox, DrawTexture, DragBar}
+import cn.lambdalib.cgui.gui.event.LeftClickEvent
+import cn.lambdalib.cgui.xml.CGUIDocument
+import cn.lambdalib.networkcall.Future.FutureCallback
+import cn.lambdalib.networkcall.s11n.StorageOption
+import cn.lambdalib.networkcall.{Future, RegNetworkCall}
+import cn.lambdalib.networkcall.s11n.StorageOption.{RangedTarget, Instance, Data, Target}
+import cn.lambdalib.util.generic.MathUtils
 import cn.lambdalib.util.helper.TickScheduler
 import cn.lambdalib.util.mc.{EntitySelectors, WorldUtils}
-import cpw.mods.fml.relauncher.Side
+import cpw.mods.fml.relauncher.{SideOnly, Side}
 import net.minecraft.block.material.Material
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.texture.IIconRegister
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
-import net.minecraft.world.World
+import net.minecraft.util.{ResourceLocation, IIcon}
+import net.minecraft.world.{World, IBlockAccess}
 
 import scala.collection.JavaConversions._
+
+object AbilityInterf {
+  val minRange = 10.0
+  val maxRange = 100.0
+}
 
 @Registrant
 @RegTileEntity
 class TileAbilityInterferer extends TileEntity {
+  import AbilityInterf._
 
   val scheduler = new TickScheduler
 
   lazy val sourceName = s"interferer@${getWorldObj.provider.dimensionId}($xCoord,$yCoord,$zCoord)"
 
-  /**
-    * TODO make user able to specify the range
-    */
-  def range = 5
+  private var enabled_ = false
+  private var placer_ : Option[String] = None
+  private var range_ = minRange
 
-  scheduler.every(10).atOnly(Side.SERVER).run(new Runnable {
+  def enabled = enabled_
+  def setEnabled(value: Boolean) = {
+    enabled_ = value
+    sync()
+  }
+
+  def range = range_
+  def setRange(value: Double) = {
+    range_ = value
+    sync()
+  }
+
+  def placer = placer_
+  def setPlacer(player: EntityPlayer) = {
+    placer_ = Some(player.getCommandSenderName)
+    sync()
+  }
+
+  // Check player in the area and interfere them
+  scheduler.every(10).atOnly(Side.SERVER).condition(new Supplier[lang.Boolean] {
+    override def get(): lang.Boolean = enabled
+  }).run(new Runnable {
     override def run() = {
       val rangeVal = range
       val players = WorldUtils.getEntities(TileAbilityInterferer.this, rangeVal, EntitySelectors.survivalPlayer)
@@ -37,18 +83,164 @@ class TileAbilityInterferer extends TileEntity {
           CPData.get(player).addInterf(sourceName, new IInterfSource {
             override def interfering(): Boolean =
               player.getDistanceSq(xCoord+0.5, yCoord+0.5, zCoord+0.5) < rangeVal * rangeVal &&
-                !TileAbilityInterferer.this.isInvalid
+                !TileAbilityInterferer.this.isInvalid &&
+                !player.capabilities.isCreativeMode &&
+                enabled
           })
       }
     }
   })
 
+  // Sync data to client
+  scheduler.every(20).atOnly(Side.SERVER).run(new Runnable {
+    override def run() = {
+      sync()
+    }
+  })
+
   override def updateEntity() = scheduler.runTick()
+
+  def sync() = if (!getWorldObj.isRemote) {
+    syncFromServer(this, range, placer.orNull, enabled)
+  }
+
+  @RegNetworkCall(side = Side.CLIENT, thisStorage = StorageOption.Option.INSTANCE)
+  private def syncFromServer(
+          @RangedTarget(range = 20) me: TileAbilityInterferer,
+          @Data range2 : java.lang.Double,
+          @Data placer2 : String,
+          @Data enabled2: java.lang.Boolean) = {
+    range_ = range2
+    placer_ = Option(placer2)
+    enabled_ = enabled2
+  }
+
+  // Network-cross modifiers
+
+  @RegNetworkCall(side = Side.SERVER, thisStorage = StorageOption.Option.INSTANCE)
+  def syncSetEnabled(@Data state: java.lang.Boolean, @Data future: Future) = {
+    println("SetEnabled")
+    setEnabled(state)
+    future.setAndSync(state)
+  }
+
+  @RegNetworkCall(side = Side.SERVER, thisStorage = StorageOption.Option.INSTANCE)
+  def syncSetRange(@Data range: java.lang.Double) = {
+    println("SetRange")
+    setRange(range)
+  }
 
 }
 
 class AbilityInterferer extends ACBlockContainer("ability_interferer", Material.rock) {
 
+  var iconOn: IIcon = null
+  var iconOff: IIcon = null
+
   override def createNewTileEntity(world: World, meta: Int) = new TileAbilityInterferer
+
+  override def registerBlockIcons(ir: IIconRegister) = {
+    iconOn = ricon(ir, "ability_interf_on")
+    iconOff = ricon(ir, "ability_interf_off")
+  }
+
+  override def onBlockPlacedBy(world : World, x : Int, y : Int, z : Int,
+                               placer : EntityLivingBase, stack : ItemStack) =
+    (placer, world.getTileEntity(x, y, z)) match {
+      case (player: EntityPlayer, interf: TileAbilityInterferer) =>
+        interf.setPlacer(player)
+      case _ =>
+    }
+
+
+  override def getIcon(world: IBlockAccess, x: Int, y: Int, z: Int, side: Int) =
+    world.getTileEntity(x, y, z) match {
+      case tile: TileAbilityInterferer => if (tile.enabled) iconOn else iconOff
+      case _ => iconOff
+    }
+
+  override def getIcon(side: Int, meta: Int) = iconOff
+
+  @SideOnly(Side.CLIENT)
+  override def onBlockActivated(world: World, x: Int, y: Int, z: Int,
+                                player: EntityPlayer, side: Int,
+                                tx: Float, ty: Float, tz: Float) =
+    if (world.isRemote) {
+      world.getTileEntity(x, y, z) match {
+        case tile: TileAbilityInterferer =>
+          if (player.capabilities.isCreativeMode ||
+            Option(player.getCommandSenderName) == tile.placer) {
+            Minecraft.getMinecraft.displayGuiScreen(new GuiAbilityInterferer(tile))
+          }
+          true
+        case _ => false
+      }
+    } else false
+
+}
+
+@SideOnly(Side.CLIENT)
+object GuiAbilityInterferer {
+  val tex_switchOn = Resources.getTexture("guis/button/button_switchon")
+  val tex_switchOff = Resources.getTexture("guis/button/button_switchoff")
+}
+
+class GuiAbilityInterferer(tile: TileAbilityInterferer) extends CGuiScreen {
+  import cn.lambdalib.cgui.ScalaCGUI._
+  import AbilityInterf._
+  import GuiAbilityInterferer._
+
+  val main = CGUIDocument.panicRead(new ResourceLocation("academy:guis/ability_interf.xml")).getWidget("window_main")
+  val switch    = main.getWidget("btn_switch")
+  val bar       = main.getWidget("btn_point")
+  val textRange = main.getWidget("text_range")
+
+  updateRange(tile.range)
+  updateState(tile.enabled)
+
+  switch.listens[LeftClickEvent](() => {
+    tile.syncSetEnabled(!tile.enabled, Future.create(new FutureCallback[Boolean] {
+      override def onReady(value: Boolean) = {
+        updateState(value)
+      }
+    }))
+  })
+
+  bar.listens[DraggedEvent](() => {
+    TextBox.get(textRange).setContent("%.1f".format(MathUtils.lerp(minRange, maxRange, DragBar.get(bar).getProgress)))
+  })
+
+  main.getWidget("btn_confirm").listens[LeftClickEvent](() => {
+    syncRange_()
+    mc.displayGuiScreen(null)
+  })
+
+  textRange.listens((w, evt: ConfirmInputEvent) => {
+    val textBox = TextBox.get(w)
+    try {
+      val dragbar = DragBar.get(bar)
+      val input = MathUtils.clampd(minRange, maxRange, textBox.content.toDouble)
+      updateRange(input)
+      syncRange_()
+    } catch {
+      case e: NumberFormatException =>
+        textBox.setContent(tile.range.toString)
+    }
+  })
+
+  gui.addWidget("main", main)
+
+  private def updateState(state: Boolean) = {
+    DrawTexture.get(switch).setTex(if (tile.enabled) tex_switchOn else tex_switchOff)
+  }
+
+  private def updateRange(input: Double) = {
+    DragBar.get(bar).setProgress((input - minRange) / (maxRange - minRange))
+    TextBox.get(textRange).setContent("%.1f".format(input))
+  }
+
+  private def syncRange_() = tile.syncSetRange(MathUtils.lerp(minRange, maxRange, DragBar.get(bar).getProgress))
+
+  override def doesGuiPauseGame = false
 
 }
