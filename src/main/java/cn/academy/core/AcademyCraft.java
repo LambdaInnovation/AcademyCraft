f227fb946c4e7fddc0b0f9066736ba543d869010
WeAthFolD <cheetah77@qq.com>
2016-01-02 22:48:19 +0800

    Prepare build for 1.0pr3_alpha. Close #201

--- src/main/java/cn/academy/core/AcademyCraft.java
+++ src/main/java/cn/academy/core/AcademyCraft.java
@@ -54,7 +54,7 @@ import org.apache.logging.log4j.Logger;
  * @author acaly, WeathFolD, KS
  *
  */
-@Mod(modid = "academy-craft", name = "AcademyCraft", version = AcademyCraft.VERSION, dependencies = "required-after:LambdaLib")
+@Mod(modid = "academy-craft", name = "AcademyCraft", version = AcademyCraft.VERSION, dependencies = "required-after:LambdaLib@1.1.0_alpha")
 @RegistrationMod(pkg = "cn.academy.", res = "academy", prefix = "ac_")
 @Registrant
 public class AcademyCraft {
@@ -62,9 +62,9 @@ public class AcademyCraft {
 	@Instance("academy-craft")
 	public static AcademyCraft INSTANCE;
 
-	public static final boolean DEBUG_MODE = true;
+	public static final boolean DEBUG_MODE = false;
 
-	public static final String VERSION = "1.0pr3_dev";
+	public static final String VERSION = "1.0pr3_alpha";
 
 	public static final Logger log = LogManager.getLogger("AcademyCraft");
 
