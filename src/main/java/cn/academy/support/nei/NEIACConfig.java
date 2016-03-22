1d484c1ae7eb35ffa6a712a6be81ca3ca039e157
WeAthFolD <cheetah77@qq.com>
2016-03-22 11:41:46 +0800

    Fixed NEI overlay shadowing UI Area
    (妈的智障

--- src/main/java/cn/academy/support/nei/NEIACConfig.java
+++ src/main/java/cn/academy/support/nei/NEIACConfig.java
@@ -7,8 +7,16 @@
 package cn.academy.support.nei;
 
 import cn.academy.core.AcademyCraft;
-import codechicken.nei.api.API;
-import codechicken.nei.api.IConfigureNEI;
+import cn.academy.core.client.ui.TechUI.ContainerUI;
+import codechicken.nei.VisiblityData;
+import codechicken.nei.api.*;
+import codechicken.nei.recipe.IRecipeHandler;
+import com.google.common.collect.Lists;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.item.ItemStack;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 
@@ -23,6 +31,42 @@ public class NEIACConfig implements IConfigureNEI {
         API.registerUsageHandler(new FusorRecipeHandler());
         API.registerRecipeHandler(new MetalFormerRecipeHandler());
         API.registerUsageHandler(new MetalFormerRecipeHandler());
+        API.registerNEIGuiHandler(new INEIGuiHandler() {
+            @Override
+            public VisiblityData modifyVisiblity(GuiContainer guiContainer, VisiblityData visiblityData) {
+                return null;
+            }
+
+            @Override
+            public Iterable<Integer> getItemSpawnSlots(GuiContainer guiContainer, ItemStack itemStack) {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public List<TaggedInventoryArea> getInventoryAreas(GuiContainer guiContainer) {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public boolean handleDragNDrop(GuiContainer guiContainer, int i, int i1, ItemStack itemStack, int i2) {
+                return false;
+            }
+
+            @Override
+            public boolean hideItemPanelSlot(GuiContainer gui, int x, int y, int w, int h) {
+                ContainerUI ui = check(gui);
+                System.out.println("Checked");
+                return ui.getGui().getTopWidget(x, y) != null;
+            }
+
+            private ContainerUI check(GuiContainer gui) {
+                if (gui instanceof ContainerUI) {
+                    return ((ContainerUI) gui);
+                } else {
+                    return null;
+                }
+            }
+        });
     }
 
     @Override
