4b773ed3a287ac2646b16270bf2b83ac38f2dfe6
WeAthFolD <cheetah77@qq.com>
2016-04-23 21:49:23 +0800

    Bump version

--- src/main/java/cn/academy/core/AcademyCraft.java
+++ src/main/java/cn/academy/core/AcademyCraft.java
@@ -44,7 +44,7 @@ import java.util.Map.Entry;
  *
  */
 @Mod(modid = "academy-craft", name = "AcademyCraft", version = AcademyCraft.VERSION,
-     dependencies = "required-after:LambdaLib@[1.2.0_a3]") // LambdaLib is currently unstable. Supports only one version.
+     dependencies = "required-after:LambdaLib@[1.2.0_a4]") // LambdaLib is currently unstable. Supports only one version.
 @RegistrationMod(pkg = "cn.academy.", res = "academy", prefix = "ac_")
 @Registrant
 @VersionUpdateUrl(repoUrl="github.com/LambdaInnovation/AcademyCraft")
@@ -55,7 +55,7 @@ public class AcademyCraft {
 
     public static final boolean DEBUG_MODE = true;
 
-    public static final String VERSION = "1.0.0_a3";
+    public static final String VERSION = "1.0.0_a4";
 
     public static final Logger log = LogManager.getLogger("AcademyCraft");
 
