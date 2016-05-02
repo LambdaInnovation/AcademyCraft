e83c6e0075c458367a2c0665b7f5cab8a342e5b3
Mastize <MastizeNone@gmail.com>
2016-05-02 20:12:27 +0800

    add some tutorial entries

--- src/main/java/cn/academy/misc/tutorial/ModuleTutorial.java
+++ src/main/java/cn/academy/misc/tutorial/ModuleTutorial.java
@@ -7,28 +7,29 @@
 package cn.academy.misc.tutorial;
 
 import cn.academy.ability.ModuleAbility;
+import cn.academy.core.ModuleCoreClient;
 import cn.academy.core.client.Resources;
 import cn.academy.core.registry.ACRecipeNamesRegistration.RegACRecipeNames;
 import cn.academy.crafting.ModuleCrafting;
 import cn.academy.energy.ModuleEnergy;
+import cn.academy.misc.ModuleMisc;
 import cn.academy.misc.tutorial.client.GuiTutorial;
+import cn.academy.support.rf.RFSupport;
 import cn.academy.terminal.App;
 import cn.academy.terminal.AppEnvironment;
 import cn.academy.terminal.AppRegistry;
 import cn.academy.terminal.ModuleTerminal;
+import cn.academy.vanilla.ModuleVanilla;
 import cn.lambdalib.annoreg.core.Registrant;
 import cn.lambdalib.annoreg.mc.RegItem;
 import cn.lambdalib.annoreg.mc.RegPostInitCallback;
 import cn.lambdalib.crafting.CustomMappingHelper.RecipeName;
 import cn.lambdalib.util.generic.RandUtils;
 import cn.lambdalib.util.helper.Color;
-import cn.lambdalib.vis.model.CompTransform;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
-
-import java.util.Optional;
 
 import static cn.academy.misc.tutorial.Conditions.*;
 import static cn.academy.misc.tutorial.PreviewHandlers.*;
@@ -49,29 +50,74 @@ public class ModuleTutorial {
     public static void initConditions() {
         defnTut("welcome");
 
-        defnTut("phase_liquid")
-            .addPreview(displayIcon("items/matter_unit/phase_liquid_mat",
-                                0, 0, 
-                                1, Color.white()))
-            .addPreview(recipes(ModuleCrafting.matterUnit));
-
-        defnTut("constraint_metal")
-                .setCondition(itemObtained(ModuleCrafting.oreConstraintMetal))
+        defnTut("ores")
+                .setCondition(itemObtained(ModuleCrafting.oreConstraintMetal)
+                        .or(itemObtained(ModuleCrafting.oreImagSil))
+                        .or(itemObtained(ModuleCrafting.oreImagCrystal))
+                        .or(itemObtained(ModuleCrafting.oreResoCrystal)))
                 .addPreview(drawsBlock(ModuleCrafting.oreConstraintMetal))
-                .addPreview(drawsItem(ModuleCrafting.ingotConst))
-                .addPreview(drawsItem(ModuleCrafting.constPlate))
-                .addPreview(recipes(ModuleCrafting.ingotConst))
-                .addPreview(recipes(ModuleCrafting.constPlate));
+                .addPreview(drawsBlock(ModuleCrafting.oreImagSil))
+                .addPreview(drawsBlock(ModuleCrafting.oreImagCrystal))
+                .addPreview(drawsBlock(ModuleCrafting.oreResoCrystal))
+                .addPreview(displayIcon("items/matter_unit/phase_liquid_mat",
+                        0, 0,
+                        1, Color.white()))
+                .addPreview(recipes(ModuleCrafting.constPlate))
+                .addPreview(recipes(ModuleCrafting.ingotImagSil))
+                .addPreview(recipes(ModuleCrafting.wafer))
+                .addPreview(recipes(ModuleCrafting.silPiece));
 
-        defnTut("crystal")
-                .setCondition(itemObtained(ModuleCrafting.crystalLow))
-                .addPreview(drawsItem(ModuleCrafting.crystalLow))
-                .addPreview(drawsItem(ModuleCrafting.crystalNormal))
-                .addPreview(drawsItem(ModuleCrafting.crystalPure))
-                .addPreview(recipes(ModuleCrafting.crystalLow))
-                .addPreview(recipes(ModuleCrafting.crystalNormal))
-                .addPreview(recipes(ModuleCrafting.crystalPure));
+        defnTut("phase_generator")
+                .setCondition(itemObtained(ModuleEnergy.phaseGen))
+                .addPreview(recipes(ModuleEnergy.phaseGen));
 
+        defnTut("solar_generator")
+                .setCondition(itemObtained(ModuleEnergy.solarGen))
+                .addPreview(recipes(ModuleEnergy.solarGen));
+
+        defnTut("wind_generator")
+                .setCondition(itemObtained(ModuleEnergy.windgenBase)
+                        .or(itemObtained(ModuleEnergy.windgenFan))
+                        .or(itemObtained(ModuleEnergy.windgenMain))
+                        .or(itemObtained(ModuleEnergy.windgenPillar)))
+                .addPreview(recipes(ModuleEnergy.windgenBase))
+                .addPreview(recipes(ModuleEnergy.windgenPillar))
+                .addPreview(recipes(ModuleEnergy.windgenMain))
+                .addPreview(recipes(ModuleEnergy.windgenFan));
+
+        defnTut("metal_former")
+                .setCondition(itemObtained(ModuleCrafting.metalFormer))
+                .addPreview(recipes(ModuleCrafting.metalFormer));
+
+        defnTut("imag_fusor")
+                .setCondition(itemObtained(ModuleCrafting.imagFusor))
+                .addPreview(recipes(ModuleCrafting.imagFusor));
+
+        defnTut("terminal")
+                .setCondition(itemObtained(ModuleTerminal.terminalInstaller))
+                .addPreview(recipes(ModuleTerminal.terminalInstaller));
+
+        defnTut("ability_developer")
+                .setCondition(itemObtained(ModuleAbility.developerPortable)
+                        .or(itemObtained(ModuleAbility.developerNormal))
+                        .or(itemObtained(ModuleAbility.developerAdvanced)))
+                .addPreview(recipes(ModuleAbility.developerPortable))
+                .addPreview(recipes(ModuleAbility.developerNormal))
+                .addPreview(recipes(ModuleAbility.developerAdvanced));
+
+        //defnTut("ability")
+
+        defnTut("energy_bridge")
+                .setCondition(itemObtained(RFSupport.rfInput)
+                        .or(itemObtained(RFSupport.rfOutput)))
+                .addPreview(recipes(RFSupport.rfInput))
+                .addPreview(recipes(RFSupport.rfOutput));
+
+        defnTut("misc");
+
+        defnTut("ability_basis");
+
+        defnTut("energy");
 
         // Add app for tutorial
         AppRegistry.register(new App("tutorial") {
