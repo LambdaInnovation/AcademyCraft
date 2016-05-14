a0cbb02f3063de10cbdd489ba01e2c324061940d
WeAthFolD <cheetah77@qq.com>
2016-05-15 01:44:55 +0800

    Bump version to 1.0.1

--- src/main/java/cn/academy/core/AcademyCraft.java
+++ src/main/java/cn/academy/core/AcademyCraft.java
@@ -54,7 +54,7 @@ public class AcademyCraft {
 
     public static final boolean DEBUG_MODE = false;
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final Logger log = LogManager.getLogger("AcademyCraft");
 
