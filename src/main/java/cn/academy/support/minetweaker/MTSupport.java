b137ff105d8fcb3cf8144bfbd4ee2d4eade62fb9
3TUSK <urey.s.knowledge@gmail.com>
2018-10-10 10:20:24 -0700

    Modernize CraftTweaker support

--- src/main/java/cn/academy/support/minetweaker/MTSupport.java
+++ src/main/java/cn/academy/support/minetweaker/MTSupport.java
@@ -1,32 +1,16 @@
 package cn.academy.support.minetweaker;
 
-import cn.academy.AcademyCraft;
-import cn.lambdalib2.registry.StateEventCallback;
-import crafttweaker.CraftTweakerAPI;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
-import net.minecraftforge.fml.common.Optional;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 /**
  * 
  * @author 3TUSK
  */
-public class MTSupport {
+public final class MTSupport {
 
-    private static final String MODID = "crafttweaker";
-
-    @StateEventCallback
-    @Optional.Method(modid=MODID)
-    private static void init(FMLInitializationEvent event) {
-        CraftTweakerAPI.registerClass(ImagFusorSupport.class);
-        CraftTweakerAPI.registerClass(MetalFormerSupport.class);
-        AcademyCraft.log.info("MineTweaker API support has been loaded.");
-    }
-
-    @Optional.Method(modid=MODID)
-    public static ItemStack toStack(IItemStack s) {
+    static ItemStack toStack(IItemStack s) {
         return CraftTweakerMC.getItemStack(s);
     }
 
