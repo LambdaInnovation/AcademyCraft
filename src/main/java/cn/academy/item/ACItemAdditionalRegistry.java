346bba7bc4c784961da17cec5b00eed59057e4ed
Paindar <16387007+Paindar@users.noreply.github.com>
2018-10-31 03:15:45 +0800

    Fix LootTable.

--- src/main/java/cn/academy/item/ACItemAdditionalRegistry.java
+++ src/main/java/cn/academy/item/ACItemAdditionalRegistry.java
@@ -1,32 +1,61 @@
 package cn.academy.item;
 
 import cn.academy.ACItems;
-import cn.academy.ability.Category;
-import cn.academy.ability.CategoryManager;
-import cn.academy.block.block.ACFluids;
-import cn.academy.support.EnergyItemHelper;
-import cn.lambdalib2.registry.StateEventCallback;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.storage.loot.conditions.RandomChanceWithLooting;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.*;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
+import net.minecraft.world.storage.loot.functions.LootFunction;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
 
 public class ACItemAdditionalRegistry {
 
-    @StateEventCallback
-    private static void init(FMLInitializationEvent ev) {
-        // TODO use loot table
-//        String[] factorAppearance = { MINESHAFT_CORRIDOR, PYRAMID_DESERT_CHEST, PYRAMID_JUNGLE_CHEST, STRONGHOLD_LIBRARY,
-//            DUNGEON_CHEST };
+    private static final ACItemAdditionalRegistry INSTANCE = new ACItemAdditionalRegistry();
+    private static final ResourceLocation[] factorAppearance = { LootTableList.CHESTS_ABANDONED_MINESHAFT,
+            LootTableList.CHESTS_DESERT_PYRAMID,
+            LootTableList.CHESTS_JUNGLE_TEMPLE,
+            LootTableList.CHESTS_STRONGHOLD_LIBRARY,
+            LootTableList.CHESTS_SIMPLE_DUNGEON };
 
-        // TODO test generation density
-//        for (String s : factorAppearance) {
-//            for (Category c : CategoryManager.INSTANCE.getCategories()) {
-//                ItemStack stack = ACItems.induction_factor.create(c);
-//                ChestGenHooks.addItem(s, new WeightedRandomChestContent(stack, 1, 1, 4));
-//            }
-//        }
+    //public static final ResourceLocation INDUCTION_FACTOR = LootTableList.register(new ResourceLocation("academy", "inject/induction_factor"));;
+    private static final LootPool pool;
+    static{
+        LootCondition conds[] = new LootCondition[0];
+        LootFunction func = new LootFunction(conds)
+        {
+            @Override
+            public ItemStack apply(ItemStack stack, Random rand, LootContext context)
+            {
+                stack.setItemDamage(rand.nextInt(4));
+                return stack;
+            }
+        };
+        LootEntry entries[] = new LootEntry[]{
+                new LootEntryItem(ACItems.induction_factor, 10, 1,
+                        new LootFunction[]{func}, conds,"academy:induction_factor"),
+                new LootEntryEmpty(90, 1, conds, "empty")
+        };
+        pool = new LootPool(entries, conds, new RandomValueRange(4, 4), new RandomValueRange(0,0), "academy:main");
+    }
+
+    private ACItemAdditionalRegistry()
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void lootLoad(LootTableLoadEvent evt) {
+        for(ResourceLocation loc:factorAppearance)
+        {
+            if (evt.getName().equals(loc)) {
+                evt.getTable().addPool(pool);
+                break;
+            }
+        }
     }
 
 }
