b8d7e59d5e1dcf695e1330eaa8a2354f50bce310
Paindar <16387007+Paindar@users.noreply.github.com>
2018-10-31 14:39:38 +0800

    Fix Advancement.FirstContact cannot be triggered at multiplay.

--- src/main/java/cn/academy/item/ItemTutorial.java
+++ src/main/java/cn/academy/item/ItemTutorial.java
@@ -2,6 +2,9 @@ package cn.academy.item;
 
 import cn.academy.advancements.ACAchievements;
 import cn.academy.client.gui.GuiTutorial;
+import cn.lambdalib2.s11n.network.NetworkMessage;
+import cn.lambdalib2.s11n.network.NetworkMessage.Listener;
+import cn.lambdalib2.s11n.network.NetworkS11nType;
 import net.minecraft.item.Item;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
@@ -16,8 +19,10 @@ import net.minecraft.world.World;
 /**
  * @author WeAthFolD
  */
+@NetworkS11nType
 public class ItemTutorial extends Item {
 
+    private static final String MSG_TRIGGER = ".item.tutorial.trigger_advance";
     @SideOnly(Side.CLIENT)
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
@@ -25,8 +30,14 @@ public class ItemTutorial extends Item {
         if(world.isRemote) {
             Minecraft.getMinecraft().displayGuiScreen(new GuiTutorial());
         }
-        ACAchievements.trigger(player, ACAchievements.aOpenTutorial.ID);
+        NetworkMessage.sendToServer(this, MSG_TRIGGER, player);
         return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+    }
+
+    @Listener(channel=MSG_TRIGGER, side = Side.SERVER)
+    public void onTrigger(EntityPlayer player)
+    {
+        ACAchievements.trigger(player, ACAchievements.aOpenTutorial.ID);
     }
     
 }
